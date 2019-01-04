1f8048ca0f849980759e3b9e7e342ceb0ecdf399
Anderson Costa <anderson.analistati@gmail.com>
2019-01-04 01:58:20 -0200

    update project

--- src/services/api.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-    baseURL: 'https://randomuser.me',
-})
-
-export default api;
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,9 @@
 import { createStore, applyMiddleware } from 'redux';
-import createSagaMiddleware from 'redux-saga';
-
 import reducers from './reducers';
-import sagas from './sagas'
 
-const sagaMiddleware = createSagaMiddleware();
-const middleware = [sagaMiddleware];
+const middleware = [];
 
 const createAppropriateStore = __DEV__ ? console.tron.createStore : createStore;
 const store = createAppropriateStore(reducers, applyMiddleware(...middleware));
-
-sagaMiddleware.run(sagas);
 
 export default store;
